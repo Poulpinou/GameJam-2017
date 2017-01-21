a7cc0bf5ae2b8c517542a687a96c5ba51be5a691
wBienfait <wbienfait@gmail.com>
2017-01-21 20:24:36 +0100

    commit miam miam

---
+++ Assets/WorldObjects/Units/Traps/Vibartor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Vibartor : Trap {
+
+	[SerializeField] GameObject _wave;
+
+	float _lastWave;
+	List<GameObject> _waves;
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Time.time - _lastWave > reloadTime)
+		{
+			SpawnWave();
+		}
+	}
+
+	void SpawnWave()
+	{
+		GameObject newWave = Instantiate(_wave);
+		newWave.transform.parent = this.transform;
+		newWave.transform.localPosition = new Vector3(0, newWave.transform.localPosition.y, 0);
+		_lastWave = Time.time;
+	}
+}
---
+++ Assets/WorldObjects/Units/Traps/VibartorWave.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VibartorWave : MonoBehaviour {
+
+	[SerializeField] float _speelLoss;
+	[SerializeField] float _waveSpeed;
+	float _timeOnSpawn;
+	float _scale;
+	// Use this for initialization
+	void Awake ()
+	{
+		_timeOnSpawn = Time.time;
+		_scale = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_scale += Time.deltaTime * _waveSpeed;
+		transform.localScale = Vector3.one * _scale;
+		if(_scale > 1)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		print("trigger");
+		if(collider.GetComponent<Enemy>() != null)
+		{
+			Enemy enemy = collider.GetComponent<Enemy>();
+			enemy.SetDamages(1);
+			enemy.GetComponent<NavMeshAgent>().speed -= _speelLoss;
+		}
+	}
+
+	private void OnTriggerExit(Collider collider)
+	{
+		if (collider.GetComponent<Enemy>() != null)
+		{
+			Enemy enemy = collider.GetComponent<Enemy>();
+			enemy.SetDamages(1);
+			enemy.GetComponent<NavMeshAgent>().speed += _speelLoss;
+		}
+	}
+}
