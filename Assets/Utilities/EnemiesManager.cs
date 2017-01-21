29df84d17f156396dc5594faec3002244f3a414f
wBienfait <wbienfait@gmail.com>
2017-01-21 18:57:49 +0100

    changes

--- Assets/Utilities/EnemiesManager.cs
+++ Assets/Utilities/EnemiesManager.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class EnemiesManager : MonoBehaviour {
 	[SerializeField] BoxCollider _spawn;
-	[SerializeField] GameObject _enemy;
+	[SerializeField] GameObject _enemyBibit;
+	[SerializeField] GameObject _enemyFatCock;
+	[SerializeField] GameObject _enemyFlyingPhallus;
 
 	// Use this for initialization
 	void Start () {
@@ -12,16 +14,38 @@ public class EnemiesManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.E))
+		if (Input.GetKeyDown(KeyCode.B))
 		{
 			SpawnEnemy(BDB.Enemies.Bibit);
+		}
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			SpawnEnemy(BDB.Enemies.FatCock);
+		}
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			SpawnEnemy(BDB.Enemies.FlyingPhallus);
 		}
 	}
 
 
 	public void SpawnEnemy(BDB.Enemies enemy)
 	{
-		GameObject newEnemy = Instantiate(_enemy);
+		GameObject newEnemy = null;
+		switch (enemy)
+		{
+			case BDB.Enemies.Bibit:
+				newEnemy = Instantiate(_enemyBibit);
+				break;
+			case BDB.Enemies.FatCock:
+				newEnemy = Instantiate(_enemyFatCock);
+				break;
+			case BDB.Enemies.FlyingPhallus:
+				newEnemy = Instantiate(_enemyFlyingPhallus);
+				break;
+			default:
+				break;
+		}
 		float distanceX = Random.Range(-_spawn.transform.lossyScale.x / 2, _spawn.transform.lossyScale.x / 2);
 		float distanceZ = Random.Range(-_spawn.transform.lossyScale.z / 2, _spawn.transform.lossyScale.z / 2);
 		newEnemy.transform.position = _spawn.transform.position + _spawn.transform.right * distanceX + _spawn.transform.forward * distanceZ;
